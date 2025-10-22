dbe84ecba74bc0119f6af7f5e8acb09b070036c1
viann <2124100011@soy.utj.edu.mx>
2025-10-22 10:40:29 -0600

    .

--- app/src/main/java/com/example/proyectomoviles/MainActivity.kt
+++ app/src/main/java/com/example/proyectomoviles/MainActivity.kt
@@ -4,28 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.proyectomoviles.layouts.BotonesOrdenados
 import com.example.proyectomoviles.layouts.DatosUsuario
-import com.example.proyectomoviles.layouts.IniciarSesionBtn
 import com.example.proyectomoviles.layouts.InicioDeSesionPreview
-import com.example.proyectomoviles.layouts.LogoAplicacion
-import com.example.proyectomoviles.ui.theme.ProyectoMovilesTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            //InicioDeSesionPreview()
-             DatosUsuario()
+            InicioDeSesionPreview()
+             //DatosUsuario()
             //BotonesOrdenados()
 
             }
